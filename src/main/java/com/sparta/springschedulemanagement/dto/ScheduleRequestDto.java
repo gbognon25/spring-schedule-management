cfa82a50e4a1223b32d9c0dd124589685fcd2230
soung90 <sounglikane@gmail.com>
2024-10-02 16:26:48 +0900

    주석 정리

--- src/main/java/com/sparta/springschedulemanagement/dto/ScheduleRequestDto.java
+++ src/main/java/com/sparta/springschedulemanagement/dto/ScheduleRequestDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class ScheduleRequestDto {
-    private String title;
+    private String title; //할알
     private String task; // 내용
     private String author; // 작성자명
     private String password; // 비밀번호
