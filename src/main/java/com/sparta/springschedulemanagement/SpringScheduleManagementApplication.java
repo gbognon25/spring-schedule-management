1d8cee0d40e35f939514dbbf3c6a3c8c36176d41
soung90 <sounglikane@gmail.com>
2024-10-02 10:11:13 +0900

    Initial commit

---
+++ src/main/java/com/sparta/springschedulemanagement/SpringScheduleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springschedulemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScheduleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringScheduleManagementApplication.class, args);
+    }
+
+}
